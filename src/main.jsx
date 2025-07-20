303a790808aae1186bed0a906b5e297dbed8a30b
casagrandeee <110943883+casagrandeee@users.noreply.github.com>
2025-07-20 13:24:42 +0100

    Add comments

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// focus start Monday
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
